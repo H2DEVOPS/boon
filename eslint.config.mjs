88f1bb5b4c5da89f70c8a57c62217aae8d9936c4
H2DEVOPS <teknik@h2entreprenad.se>
2026-02-17 01:38:29 +0100

    Update ESLint configuration to include additional ignored files, enhancing linting process by excluding specific config files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,16 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["node_modules/", "dist/", "coverage/", "**/*.min.js"] },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-      },
-    },
-  }
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "coverage/",
+      "**/*.min.js",
+      "eslint.config.mjs",
+      "vitest.config.ts",
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended
 );
